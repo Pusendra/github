dc610eb0b5b886486dbde612047024213a44a8da
Pusendra Chaudhary <cpusendra@gmail.com>
2019-12-01 21:12:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const makecommit = n=>{
     if(n===0) return simpleGit().push()  ;  
     const x =random.int(0,50);
     const y = random.int(0,6);
-    const DATE = moment().subtract(7,'d')./*.add(1,'d')
-    .add(x, 'w').add(y, 'd')*/format();
+    const DATE = moment().subtract(1,'y').add(1,'d')
+    .add(x, 'w').add(y, 'd').format();
     const data = {date: DATE};
     console.log(DATE);
     jsonfile.writeFile(FILE_PATH, data, ()=>{simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, 
